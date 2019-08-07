6ad6670c75b67289d747602ceb13e6e9bf4b6808
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2019-08-07 12:24:24 +0300

    Dummed down real importer to just import record logger

--- src/config.js
+++ src/config.js
@@ -29,13 +29,3 @@
 import {Utils} from '@natlibfi/melinda-commons';
 
 const {readEnvironmentVariable, parseBoolean} = Utils;
-
-export const CATALOGER_ID = readEnvironmentVariable('CATALOGER_ID', {defaultValue: 'IMP_HELMET'});
-
-export const SRU_URL = readEnvironmentVariable('SRU_URL');
-
-export const RECORD_LOAD_URL = readEnvironmentVariable('RECORD_LOAD_URL');
-export const RECORD_LOAD_API_KEY = readEnvironmentVariable('RECORD_LOAD_API_KEY');
-export const RECORD_LOAD_LIBRARY = readEnvironmentVariable('RECORD_LOAD_LIBRARY');
-
-export const NOOP_MELINDA_IMPORT = readEnvironmentVariable('NOOP_MELINDA_IMPORT', {defaultValue: false, format: parseBoolean});
--- src/import.js
+++ src/import.js
@@ -29,55 +29,40 @@
 /* eslint-disable import/default */
 
 import {MarcRecord} from '@natlibfi/marc-record';
-import {Utils, RecordMatching, Datastore} from '@natlibfi/melinda-commons';
-import {RECORD_IMPORT_STATE} from '@natlibfi/melinda-record-import-commons';
-import {
-	CATALOGER_ID, SRU_URL, NOOP_MELINDA_IMPORT,
-	RECORD_LOAD_URL, RECORD_LOAD_API_KEY, RECORD_LOAD_LIBRARY
-} from './config';
+import {Utils} from '@natlibfi/melinda-commons';
+
 
 const {createLogger, getRecordTitle, getRecordStandardIdentifiers} = Utils;
-const {createSimpleBibService: createMatchingService} = RecordMatching;
-const {createService: createDatastoreService} = Datastore;
 
+// Does this need to be true?
 MarcRecord.setValidationOptions({subfieldValues: false});
 
 export default function () {
 	const Logger = createLogger();
-	const MatchingService = createMatchingService({sruURL: SRU_URL, maxCandidatesPerQuery: 1});
-	const DatastoreService = createDatastoreService({
-		sruURL: SRU_URL,
-		recordLoadURL: RECORD_LOAD_URL,
-		recordLoadApiKey: RECORD_LOAD_API_KEY,
-		recordLoadLibrary: RECORD_LOAD_LIBRARY
-	});
 
 	return async message => {
-		if (NOOP_MELINDA_IMPORT) {
-			Logger.log('info', 'NOOP_MELINDA_IMPORT is set. Not importing anything');
-		} else {
-			Logger.log('info', 'Importing record to Melinda');
-		}
+		Logger.log('info', 'Not importing anything to Melinda, just logging!');
 
 		const record = new MarcRecord(JSON.parse(message.content.toString()));
 		const title = getRecordTitle(record);
 		const standardIdentifiers = getRecordStandardIdentifiers(record);
+		const recordFields = record.fields;
 
-		Logger.log('debug', 'Trying to find matches for record...');
-		const matches = await MatchingService.find(record);
-
-		if (matches.length > 0) {
-			return {status: RECORD_IMPORT_STATE.DUPLICATE, metadata: {matches, title, standardIdentifiers}};
-		}
-
-		if (NOOP_MELINDA_IMPORT) {
-			return {status: RECORD_IMPORT_STATE.SKIPPED, metadata: {title, standardIdentifiers}};
-		}
-
-		Logger.log('info', 'Importing record to datastore...');
-		const id = await DatastoreService.create({record, cataloger: CATALOGER_ID});
-
-		Logger.log('info', `Created new record ${id}`);
-		return {status: RECORD_IMPORT_STATE.CREATED, metadata: {id, title, standardIdentifiers}};
+		// Logs record and it's subfields to info channel
+		// Possibility to add enviromental variable to change log location
+		// Logging to file needs new function to Melinda-commons-js/utils
+		Logger.log('info', 'Got record for importing:');
+		Logger.log('info', 'Record title: ' + title);
+		Logger.log('info', 'Standard identifiers: ' +standardIdentifiers);
+		Logger.log('info', 'Records fields to be imported:');
+		recordFields.forEach(field => {
+			Logger.log('info', 'Tag: ' + field.tag)
+			Logger.log('info', 'Subfields:')
+			field.subfield.forEach(sub => {
+				Logger.log('info', 'Code: ' + sub.code)
+				Logger.log('info', 'Value: ' + sub.value)
+			})
+			Logger.log('info', '---------')
+		})
 	};
 }
