2b13c792eb3d7d763ded4aa04c672ee40e1aba1a
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2022-04-05 09:54:24 +0300

    adapt to new records format in melinda-record-api

--- src/handleBulkResult.js
+++ src/handleBulkResult.js
@@ -1,24 +1,43 @@
 // {
-//   "correlationId":"FOO",
-//   "cataloger":"xxx0000",
-//   "oCatalogerIn": "xxx0000",
-//   "operation":"CREATE",
-//   "queueItemState":"DONE",
-//   "handledRecords": [
-//    {"status": "CREATED", "melindaId":"001234567", "title": "example", "standardIdentifiers": ["978-952-373-345-9"], "sourceId": "(MALLI)0239734", "blobf001": "000000001"},
-//    {"status": "UPDATED", "melindaId":"001234567", "title": "example", "standardIdentifiers": ["978-952-373-345-9"], "sourceId": "(MALLI)0239734", "blobf001": "001234567"}
+//   "correlationId": "a109ab03-3645-4916-b5c6-4b80c7148c04",
+//   "cataloger": "LOAD_IMP",
+//   "oCatalogerIn": "LOAD-IMP",
+//   "operation": "CREATE",
+//   "operations": [
+//     "CREATE"
 //   ],
-//   "rejectedRecords": [
-//    {"status": "NOT_FOUND", "melindaId":"001234567", "title": "example", "standardIdentifiers": ["978-952-373-345-9"], "sourceId": "(MALLI)0239734", "blobf001": "001234567"},
-//    {"status": "CONFLICT", "melindaId":"001234567", "message": "CAT-missmatch", "title": "example", "standardIdentifiers": ["978-952-373-345-9"], "sourceId": "(MALLI)0239734", "blobf001": "001234567"},
-//    {"status": "ERROR", "message": "example", "title": "example", "standardIdentifiers": ["978-952-373-345-9"], "sourceId": "(MALLI)0239734", "blobf001": "001234567"},
-//    {"status": "INVALID", "message": "example", "title": "example", "standardIdentifiers": ["978-952-373-345-9"], "sourceId": "(MALLI)0239734", "blobf001": "001234567"},
-//    {"status": "UNPROCESSABLE_ENTITY", "message": "example", "title": "example", "standardIdentifiers": ["978-952-373-345-9"], "sourceId": "(MALLI)0239734", "blobf001": "001234567"},
-//    {"status": "DUPLICATE", "title": "example", "standardIdentifiers": ["978-952-373-345-9"], "sourceId": "(MALLI)0239734", "blobf001": "001234567", "dublicateIds": ["001234567"]}
-//   ],
-//   "errorStatus": "",
-//   "errorMessage": "",
-// }
+//   "operationSettings": {
+//     "prio": false,
+//     "noStream": true,
+//     "noop": true,
+//     "unique": true,
+//     "merge": true,
+//     "validate": true,
+//     "failOnError": false
+//   },
+//   "contentType": "application/json",
+//   "queueItemState": "DONE",
+//   "importJobState": {
+//     "CREATE": "EMPTY",
+//     "UPDATE": "EMPTY"
+//   },
+//   "creationTime": "2022-04-04T14:03:24.841Z",
+//   "modificationTime": "2022-04-04T14:04:02.314Z",
+//   "records": [
+//     {
+//       "melindaId": "000000000",
+//       "recordMetadata": {
+//         "sourceId": "(FI-MELINDA)000999998",
+//         "blobSequence": 1,
+//         "title": "Punokset puikoille",
+//         "standardIdentifiers": [
+//           "978-951-692-678-3",
+//           "A1258209"
+//         ]
+//       },
+//       "status": "CONFLICT",
+//       "message": "Modification history mismatch (CAT)"
+//     },
 
 import {BLOB_STATE} from '@natlibfi/melinda-record-import-commons';
 import createDebugLogger from 'debug';
@@ -32,43 +51,55 @@ export async function handleBulkResult(riApiClient, blobId, bulkImportResults) {
   const {importResults} = processingInfo;
 
   debug('handleBulkresult Processing records');
-  const handledRecords = [...await processRecordData(bulkImportResults.handledRecords), ...await processRecordData(bulkImportResults.rejectedRecords)];
+  const records = await processRecordData(bulkImportResults.records);
 
   if (importResults.queueItemState === 'ERROR') {
     await riApiClient.setAborted({id: blobId});
-    return handledRecords;
+    return records;
   }
 
   await riApiClient.updateState({id: blobId, state: BLOB_STATE.PROCESSED});
-  return handledRecords;
+  return records;
 
   async function processRecordData(recordsData, handledRecords = []) {
-    const [handledRecord, ...rest] = recordsData;
+    const [record, ...rest] = recordsData;
 
-    if (handledRecord === undefined) {
+    if (record === undefined) {
       return handledRecords;
     }
 
-    const {status, melindaId = '', title, standardIdentifiers, sourceId, message = '', dublicateIds = []} = handledRecord;
-    if (importResults.some(result => result.status === status && result.metadata.title === title)) {
+    const recordData = recordDataBuilder(record);
+    // To be done remove queued item from blob
+
+    if (importResults.some(result => result.status === recordData.status && result.metadata.title === recordData.metadata.title)) {
       return processRecordData(rest, handledRecords);
     }
-
-    const recordData = {
-      status,
-      metadata: {
-        id: melindaId,
-        title,
-        standardIdentifiers,
-        sourceId,
-        message,
-        dublicateIds
-      }
-    };
 
     debug(`Record data: ${JSON.stringify(recordData)}`);
 
     await riApiClient.setRecordProcessed({id: blobId, ...recordData});
     return processRecordData(rest, [...handledRecords, recordData]);
+  }
+
+  function recordDataBuilder(record) {
+    const {status, message, dublicateIds, melindaId, recordMetadata = {}} = record;
+    const {sourceId, title, standardIdentifiers} = recordMetadata;
+
+    const metadata = {
+      id: melindaId,
+      title,
+      standardIdentifiers,
+      sourceId,
+      message
+    };
+
+    // eslint-disable-next-line functional/immutable-data
+    Object.keys(metadata).forEach(key => metadata[key] === undefined && delete metadata[key]);
+
+    if (dublicateIds) {
+      return {status, dublicateIds, metadata};
+    }
+
+    return {status, metadata};
   }
 }
