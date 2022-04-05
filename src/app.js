6c1f0610da330173b390dc5071f2dfc8ced92a5c
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2022-04-05 10:22:41 +0300

    fix logging

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ export async function startApp(config, riApiClient, melindaApiClient, transforme
       return pollResults;
     }
 
-    debug(`Current Melinda rest api item status: ${pollResults.queueItemState}, handled succesfully: ${pollMelindaRestApi.handledIds.length}, rejected: ${pollMelindaRestApi.rejectedIds.length}`);
+    debug(`Current Melinda rest api item status: ${pollResults.queueItemState}, records: ${pollMelindaRestApi.records.length}`);
     await setTimeoutPromise(1000);
 
     return pollResultHandling(melindaApiClient, recordImportBlobId, melindaRestApiCorrelationId);
